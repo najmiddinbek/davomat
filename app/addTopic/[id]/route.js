91e4028f7a8010e640329616d9419d28a6635168
najmiddin <turgunpolatov3gmail.com>
2023-10-30 20:55:48 +0500

    commit

--- app/addTopic/[id]/route.js
+++ app/addTopic/[id]/route.js
@@ -1,5 +1,5 @@
-import connectMongoDB from "@/libs/mongodb";
-import Topic from "@/models/topic";
+import connectMongoDB from "../../../lib/mongodb";
+import Topic from "../../../models/topic";
 import { NextResponse } from "next/server";
 
 export async function PUT(request, { params }) {
